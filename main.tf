3bb37b4418892e1ece1d194f5727eae0acf29c04
ZaeroX <ferruda@gmail.com>
2021-11-27 01:02:03 -0300

    added main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "West Europe"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name                = "weatherapi"
+    location            = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+
+    ip_address_type     = "public"
+    dns_name_label      = "binarythistlewavf"
+    os_type             = "Linux"
+
+    container {
+        name            = "weatherapi"
+        image           = "zaerox/weatherapi:v2"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+    }
+}
